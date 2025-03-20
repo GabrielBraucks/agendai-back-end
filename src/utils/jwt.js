c7ee52670574ec72d3c45726ec41713d33089887
Gabriel M. Braucks <braucks.codigos@gmail.com>
2025-03-20 15:38:23 -0400

    feat: Novo diretório src/utils/jwt.js com a configuração da biblioteca 'jsonwebtoken'. JWT_SECRET adicionado ao .env e .env.example.

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,27 @@
+require('dotenv').config;
 const jwt = require('jsonwebtoken');
 
-function generateToken({ id, nome, role }) {
-    return jwt.sign({ id, nome, role }, process.env.JWT_SECRET, { expiresIn: '2min' });
+const jwtSecret = `${process.env.JWT_SECRET}`;
+
+function generateToken({ id, cnpj, nome, role }) {
+    return jwt.sign({ id, cnpj, nome, role }, jwtSecret, { expiresIn: '2min' });
 }
 
-module.exports = generateToken;
+function authJwt(req, res, next) {
+    const token = req.header('Authorization')?.split(' ')[1]; // Pega o token do cabeçalho
+
+    if (!token) {
+        return res.status(401).json({ error: 'Acesso negado. Token ausente.' });
+    }
+
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Token inválido ou expirado.' });
+        }
+        
+        req.user = decoded; // Adiciona o usuário no request
+        next();
+    });
+}
+
+module.exports = { generateToken, authJwt };
