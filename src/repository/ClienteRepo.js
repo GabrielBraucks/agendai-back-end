c02698e1611e4bf32e0452518de3ff3b738ba283
Gabriel M. Braucks <braucks.codigos@gmail.com>
2025-04-04 21:09:51 -0400

    feat: ClienteRepo.js adicionado ao projeto, ainda faltam alterações e outras funções.

---
+++ src/repository/ClienteRepo.js
@@ -0,0 +1,32 @@
+const knex = require('../database/knexSetup').db;
+
+class ClienteRepo {
+    static async register({ cpf, email, nome, telefone, senha }) {
+        await knex('Cliente').insert({ cpf, email, nome, telefone, senha });
+    }
+
+    static async deleteById(id) {
+        return await knex('Cliente').where({ id }).delete();
+    }
+
+    static async getByEmail(email) {
+        return await knex('Cliente').where({ email }).first();
+    }
+
+    // Para fazer o GetByEmpresa será necessário trabalhar os agendamentos
+
+    static async getOne(id) {
+        return await knex('Cliente')
+            .where({ 'Cliente.id': id })
+            .select(
+                'Cliente.id',
+                'Cliente.cpf',
+                'Cliente.email',
+                'Cliente.nome',
+                'Cliente.telefone',
+                'Cliente.tipo'
+            ).first();
+    }
+}
+
+module.exports = ClienteRepo;
